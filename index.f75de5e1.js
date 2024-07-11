71f759778d8fd4d5e873b91b08d2dba88da62878
NataliaDavida <nataliadavida88@gmail.com>
2024-07-11 20:52:07 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const ul = document.querySelector("ul");
+function sortList(a, b) {
+    const salaryA = Number(a.dataset.salary.replace(/[^0-9]/g, ""));
+    const salaryB = Number(b.dataset.salary.replace(/[^0-9]/g, ""));
+    return salaryB - salaryA;
+}
+function getEmployees() {
+    const elements = [
+        ...document.querySelectorAll("[data-salary]")
+    ];
+    const sorted = elements.sort(sortList);
+    sorted.forEach((e)=>ul.appendChild(e));
+}
+getEmployees();
+
+//# sourceMappingURL=index.f75de5e1.js.map
